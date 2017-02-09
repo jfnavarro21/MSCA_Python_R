39aec2e8216896d59988493aa4364f8451bef5ea
jfnavarro <jfnavarro@aol.com>
2017-02-09 08:16:25 -0600

    Class project part 1 task 0 through 4 complete

---
+++ Python2ForStreamingAnalytics/Class_Project_Part1/Task_0_to_Task_4a.py
@@ -0,0 +1,105 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Task 0:
+img = cv2.imread('mario3.png', cv2.IMREAD_COLOR)
+# Apply the threshold function
+ret,thresh = cv2.threshold(img, 127,255,cv2.THRESH_BINARY)
+# display the image
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Task 1a: Display Mario Gray-Scale
+
+grayMario = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('image', grayMario)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Task 1b: Reducing the background noise of a video
+
+blurMario = cv2.GaussianBlur(grayMario, (9,9), 0)
+cv2.imshow('image', blurMario)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Task 1c: Image differencing
+# Create the gray and blurry mario for mario2.png
+img4 = cv2.imread('mario4.png', cv2.IMREAD_COLOR)
+ret,thresh = cv2.threshold(img4, 127,255,cv2.THRESH_BINARY)
+grayMario4 = cv2.cvtColor(img4, cv2.COLOR_BGR2GRAY)
+blurMario4 = cv2.GaussianBlur(grayMario4, (9,9), 0)
+cv2.imshow('image', blurMario4)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# Use absdiff function to show the pixel differences from the two marios
+diffMario = cv2.absdiff(np.float32((blurMario)), np.float32((blurMario4)))
+cv2.imshow('image', diffMario)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Task 2 & 3: Something is moving on the video - Part 1 and 2
+import pandas as pd
+# Open a video descriptor
+feed_descriptor = cv2.VideoCapture('train_training.mp4')
+# set variables and create empty lists
+roi_thresh = 0.05
+history = pd.DataFrame(columns=['Detected left', 'Detected right'])
+history_length = 70
+motion_fractions = []
+motion_thresh = 35
+running_avg = None
+alpha = 0.2
+while(feed_descriptor.isOpened()):
+    # Read frame by frame
+    current_frame = feed_descriptor.read()[1]
+
+    if current_frame is not None:
+        gray_current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        smooth_current_frame = cv2.GaussianBlur(gray_current_frame, (9, 9), 0)
+
+        if running_avg is None:
+            # If this is the first frame, set running_avg to current frame
+            running_avg = np.float32(smooth_current_frame)
+
+        else:
+            # diff_frame is the absolute difference between the current frame and the running weighted average of previous frames
+            diff_frame = cv2.absdiff(np.float32((smooth_current_frame)), np.float32((running_avg)))
+            # Weights the current frame along with the previous frames
+            aw = cv2.accumulateWeighted(np.float32(smooth_current_frame), running_avg, alpha)
+            # for all pixels above "motion_thresh" set to 1, otherwise set to 0
+            _, subtracted = cv2.threshold(diff_frame, motion_thresh, 1, cv2.THRESH_BINARY)
+            # motion_fraction is the percentage of pixels on the entire framethat are moving
+            motion_fraction = (sum(sum(subtracted))/(subtracted.shape[0]*subtracted.shape[1]))
+            motion_fractions.append(motion_fraction)
+
+   # Check if current frame is not different from NONE
+    elif current_frame is None:
+        break
+
+# Plot the motion fractions/percent of pixels that change
+plt.plot(np.array(motion_fractions))
+plt.ylabel('Number of pixels that change')
+plt.axis([0,60,0,0.3])
+plt.show()
+# Print a gray frame and a thresholded frame
+cv2.imshow('Gray', gray_current_frame)
+cv2.imshow('Thresholded difference', subtracted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# Task 4: Where this train goes?
+
+# Task 4a: Define ROIs
+
+# Display the two regions of interest
+ROI = cv2.rectangle(smooth_current_frame, (400,400),(250,300),(0,255,0),3)
+ROI2 = cv2.rectangle(ROI, (600,400),(450,300),(0,5,0),3)
+cv2.imshow('ROI2', ROI2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
---
+++ Python2ForStreamingAnalytics/Class_Project_Part1/Task_4b.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Task 4-b Use ROIs and apply the same function to dectect movement
+
+# Open a video descriptor
+feed_descriptor = cv2.VideoCapture('train_training.mp4')
+# set variables and create empty lists
+roi_thresh = 0.05
+motion_fractions = []
+motion_fractions2 = []
+motion_thresh = 35
+running_avg = None
+running_avg2 = None
+alpha = 0.2
+
+while(feed_descriptor.isOpened()):
+    # Read frame by frame
+    current_frame = feed_descriptor.read()[1]
+    # if current_frame has a value, transform it to grayscale and blur the frame
+    if current_frame is not None:
+        gray_current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+        smooth_current_frame = cv2.GaussianBlur(gray_current_frame, (9, 9), 0)
+
+        if running_avg is None:
+            # If this is the first frame, set running_avg to current frame
+            running_avg = np.float32(smooth_current_frame[250:400, 300:400])
+            running_avg2 = np.float32(smooth_current_frame[250:400, 500:600])
+
+        else:
+            # diff_frame is the absolute difference between the current frame and the running weighted average of previous frames
+            diff_frame = cv2.absdiff(np.float32((smooth_current_frame[250:400, 300:400])), np.float32((running_avg)))
+            diff_frame2 = cv2.absdiff(np.float32((smooth_current_frame[250:400, 500:600])), np.float32((running_avg2)))
+            # accumulateWeighted averages the current frame along with the previous frames in a weighted manner
+            aw = cv2.accumulateWeighted(np.float32(smooth_current_frame[250:400, 300:400]), running_avg, alpha)
+            aw2 = cv2.accumulateWeighted(np.float32(smooth_current_frame[250:400, 500:600]), running_avg2, alpha)
+            # threshold function will transfrom the pixel to white if the difference is above motion_thresh value,
+            # it will transorm the pixel to black if it is below the threshold
+            _, subtracted = cv2.threshold(diff_frame, motion_thresh, 1, cv2.THRESH_BINARY)
+            _, subtracted2 = cv2.threshold(diff_frame2, motion_thresh, 1, cv2.THRESH_BINARY)
+            # motion_fraction is the percentage of pixels on the entire framethat are moving
+            motion_fraction = (sum(sum(subtracted))/(subtracted.shape[0]*subtracted.shape[1]))
+            # Adds each motion fraction to a list called motion_fractions
+            motion_fractions.append(motion_fraction)
+            motion_fraction2 = (sum(sum(subtracted2)) / (subtracted2.shape[0] * subtracted2.shape[1]))
+            motion_fractions2.append(motion_fraction2)
+
+    # Check if current frame is not different from NONE
+    elif current_frame is None:
+        break
+
+# commented out print statements to check values of different lists in the process
+#print(diff_frame)
+#print(motion_fractions)
+#print(len(motion_fractions))
+#print(diff_frame2)
+#print(motion_fractions2)
+#print(len(motion_fractions2))
+
+# plot the % of pixels that change for both ROIs
+plt.plot(np.array(motion_fractions), 'b')
+plt.plot(np.array(motion_fractions2),'r')
+plt.ylabel('Number of pixels that change')
+plt.axis([0,60,0,0.9])
+plt.show()
+
+
